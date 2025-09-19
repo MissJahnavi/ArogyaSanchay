4f192b85703000c7b6816494f063122e85315f3c
Jahnavi Mishra <165285980+MissJahnavi@users.noreply.github.com>
2025-09-20 00:05:00 +0530

    Update healthRecord.js

--- backend/models/healthRecord.js
+++ backend/models/healthRecord.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const HealthRecordSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    type: { type: String, enum: ["doctor_visit", "Lab-report", "vaccination", "prescription"], required: true },
+    type: { type: String, enum: ["doctor_visit", "Lab-report", "Vaccination", "Prescription"], required: true },
     title: { type: String, required: true },
     description: { type: String },
     doctor: {
